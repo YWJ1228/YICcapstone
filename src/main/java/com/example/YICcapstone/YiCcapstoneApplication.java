4d69b2febb571d719ddf2fde6ebe4ab6546ce362
TaeHoonIm <xogns9647@naver.com>
2023-09-09 18:42:02 +0900

    Init Commit

---
+++ src/main/java/com/example/YICcapstone/YiCcapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.YICcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiCcapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiCcapstoneApplication.class, args);
+	}
+
+}
