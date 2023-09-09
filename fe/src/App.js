0758801359c66d2ba063fd89a31aec61d31ae199
JunSeong Choi <114660053+castlechoi@users.noreply.github.com>
2023-09-09 19:07:56 +0900

    test
    테스트

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
+import Card from 'react-bootstrap/Card';
 import './App.css';
+// npm install -g react-scripts
+// npm install react-bootstrap bootstrap
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card> Hello World!</Card>
   );
 }
 
