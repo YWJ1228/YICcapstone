4467867a2750da8435e99ccb20c3efd7ab7ba88d
YWJ1228 <wj122800@gmail.com>
2023-09-21 10:57:19 +0900

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Member(email, password, name, nickname, birth, sex, role, created_at)
+VALUES('test1@test.com', '1234', 'Kim', 'KimNick', '990101', 1, 'USER', '2023-09-21 10:54:00.356102');
+
+INSERT INTO Member(email, password, name, nickname, birth, sex, role, created_at)
+VALUES('test2@test.com', 'YICpw', 'YIC', 'YICNick', '991201', 1, 'USER', '2023-09-21 10:55:00.356102');
