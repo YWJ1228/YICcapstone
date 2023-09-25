9f6c659f0b0386320d14a0618591d51b7bf67be2
TaeHoonIm <xogns9647@naver.com>
2023-09-25 16:34:23 +0900

    6-7주차 상품 리뷰 관심리스트 음성 모델 좋아요 db설계
    #11

---
+++ src/main/java/com/example/YICcapstone/domain/purchase/domain/VoiceModelPurchase.java
@@ -0,0 +1,32 @@
+package com.example.YICcapstone.domain.purchase.domain;
+
+
+import com.example.YICcapstone.domain.member.entity.Member;
+import com.example.YICcapstone.domain.voicemodel.domain.VoiceModel;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class VoiceModelPurchase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "voice_model_id")
+    private VoiceModel voiceModel;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column
+    private LocalDateTime purchaseAt = LocalDateTime.now();
+}
---
+++ src/main/java/com/example/YICcapstone/domain/wish/domain/VoiceModelWish.java
@@ -0,0 +1,32 @@
+package com.example.YICcapstone.domain.wish.domain;
+
+import com.example.YICcapstone.domain.member.entity.Member;
+import com.example.YICcapstone.domain.voicemodel.domain.VoiceModel;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class VoiceModelWish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "voice_model_id")
+    private VoiceModel voiceModel;
+
+    @Column
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
