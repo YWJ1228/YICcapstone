9ac1187e1227d62defba3c3cf9c91c75e39c7b7a
TaeHoonIm <xogns9647@naver.com>
2023-10-30 05:54:55 +0900

    인기순위 쿼리 수정
    #33

--- src/main/java/com/example/YICcapstone/domain/voicemodel/repository/VoiceModelRepository.java
+++ src/main/java/com/example/YICcapstone/domain/voicemodel/repository/VoiceModelRepository.java
@@ -10,14 +10,23 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VoiceModelRepository extends JpaRepository<VoiceModel, Long> {
     Page<VoiceModel> findAllByOrderByUploadedAtDesc(Pageable pageable);
-    @Query("SELECT vm FROM VoiceModel vm ORDER BY vm.viewCount + (vm.purchaseCount * 10) DESC, vm.uploadedAt DESC")
+    @Query("SELECT vm, (vm.viewCount + (vm.purchaseCount * 10) + (COUNT(vmp) * 5)) AS popularity " +
+            "FROM VoiceModel vm " +
+            "LEFT JOIN VoiceModelPreference vmp ON vm.id = vmp.voiceModel.id " +
+            "GROUP BY vm " +
+            "ORDER BY popularity DESC")
     Page<VoiceModel> findAllByOrderByScoreAndUploadedAtDesc(Pageable pageable);
     Page<VoiceModel> findAllByOrderByPriceDescUploadedAtDesc(Pageable pageable);
     Page<VoiceModel> findAllByOrderByPriceAscUploadedAtDesc(Pageable pageable);
 
     // 카테고리 분류 리스트
     Page<VoiceModel> findAllByCategoryOrderByUploadedAtDesc(String category, Pageable pageable);
-    @Query("SELECT vm FROM VoiceModel vm WHERE vm.category = ?1 ORDER BY vm.viewCount + (vm.purchaseCount * 10) DESC, vm.uploadedAt DESC")
+    @Query("SELECT vm, (vm.viewCount + (vm.purchaseCount * 10) + (COUNT(vmp) * 5)) AS popularity " +
+            "FROM VoiceModel vm " +
+            "LEFT JOIN VoiceModelPreference vmp ON vm.id = vmp.voiceModel.id " +
+            "WHERE vm.category = :category " +
+            "GROUP BY vm " +
+            "ORDER BY popularity DESC")
     Page<VoiceModel> findAllByCategoryOrderByScoreAndUploadedAtDesc(String category, Pageable pageable);
     Page<VoiceModel> findAllByCategoryOrderByPriceDescUploadedAtDesc(String category, Pageable pageable);
     Page<VoiceModel> findAllByCategoryOrderByPriceAscUploadedAtDesc(String category, Pageable pageable);
